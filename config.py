187109abd4bf9f8f25e79c11fb988666b900bfd6
thanhbao0390 <46698469+thanhbao0390@users.noreply.github.com>
2023-06-03 10:08:28 +0700

    up

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+# IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres:123456@localhost:5432/dbname'
