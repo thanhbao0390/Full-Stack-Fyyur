efd08270cd7c5be48dde9006657c07339330ab8c
thanhbao0390 <46698469+thanhbao0390@users.noreply.github.com>
2023-06-04 22:01:27 +0700

    up

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
-from wtforms.validators import DataRequired, AnyOf, URL
+from wtforms.validators import DataRequired, AnyOf, URL, Optional
 
 state_choices = [
     ('AL', 'AL'),
